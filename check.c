b4867eee44f59c10bd8b54bee20de210529ad9b2
JunSuHeo <gjwns95@naver.com>
2018-11-12 23:21:15 +0900

    Define check_reg(), check_symbol

--- check.c
+++ check.c
@@ -1,28 +1,88 @@
 #include "myas.h"
 
-enum symbol{FALSE, REG, IMM, MEM, DISP};
+enum symbol{IMM, MEM, REG};
+enum reg{EAX, ECX, EDX, EBX, ESP, EBP, ESI, EDI};
+#define FALSE -1
+
+int check_hexa(char *str, int start, int finish)
+{
+	while(idx < strlen(str)){
+		if(str[idx] == '0' || str[idx] == '1' || str[idx] == '2' || str[idx] == '3' ||
+		   str[idx] == '4' || str[idx] == '5' || str[idx] == '6' || str[idx] == '7' ||
+		   str[idx] == '8' || str[idx] == '9' || str[idx] == 'a' || str[idx] == 'b' ||
+		   str[idx] == 'c' || str[idx] == 'd' || str[idx] == 'e' || str[idx] == 'f'  )
+			start++;
+		else break;
+	}
+
+	if(start != finish)
+		return FALSE;
+
+	else return 1;
+}
+
+int check_reg(char *str, int idx)
+{
+	if(str[idx] == '%' && str[idx + 1] == 'e'){
+		if(str[idx + 3] == 'x'){
+			if(str[idx + 2] == 'a')
+				return EAX;
+			else if(str[idx + 2] =='c')
+				return ECX;
+			else if(str[idx + 2] == 'd')
+				return EDX;
+			else if(str[idx + 2] == 'b')
+				return EBX;
+			else return FALSE;
+		}
+		else if(str[idx + 3] == 'p'){
+			if(str[idx + 2] == 's')
+				return ESP;
+			else if(str[idx + 2] =='b'
+				return EBP;
+			else return FALSE;
+		}
+		else if(str[idx + 3] == 'i'){
+			if(str[idx + 2] == 's')
+				return ESI;
+			else if(str[idx + 2] == 'd')
+				return EDI;
+			else return FALSE;
+		}
+		else return FALSE;
+	}
+	else return FALSE;
+}
 
 int check_symbol(char *str)
 {
 	int last = strlen(str) - 1;
 
-	if(str[0] == '$')	//Immediate = $~~~~~~
-		return IMM;
-
-	else if(str[0] == '(' && str[1] == '%' && str[2] == 'e' && str[5] == ')')	//(%eax), (%ebx) ... = Memory
+	if(str[0] == '$'){	//Immediate = $~~~~~~
+		if(check_hexa(str, 1, last) != FALSE)
+			return IMM;
+		else
+			return FALSE;
+	}
+	
+	else if(str[0] == '('&& check_reg(str, 1) != FALSE && str[5] == ')')	//(%eax), (%ebx) ... = Memory
+		return MEM;
+	
+	else if(str[0] == '0' && str[1] == 'x'&& check_hexa(str, 2, last-6) != FALSE	//0x~~~(%edx) = Memory
+		 && str[last - 5] == '(' && str[last - 4] == '%' && str[last - 3] == 'e' && str[last] == ')')
 		return MEM;
 
-	else if(str[0] == '0' && str[1] == 'x')		//0x~~~~~ = Memory
-		return MEM;
-
-	else if(str[0] == '0' && str[1] == 'x' && str[last - 5] == '(' && str[last - 4] == '%'	//0x~~(%eax) = Memory
-		 && str[last - 3] == 'e' && str[last] == ')')
+	else if(str[0] == '0' && str[1] == 'x' && check_hexa(str, 2))	//0x~~~ = Memory
 		return MEM;
 
 	else if(str[0] == '-' && str[1] == '0' && str[2] == 'x' && str[last -5] == '(' &&	//-0x~~(%eax) = Memory
-		str[last - 4] == '%' && str[last - 3] == 'e' && str[last] == ')')
+		check_reg(str, last - 4) != FALSE && str[last] == ')')
 		return MEM;
 	
+	else if(check_reg(str, 0) != FALSE)	//%eax, %ebx ... = Register
+		return REG;
+	
+	else return FALSE;
 }
 
 
@@ -54,6 +114,8 @@ int is_valid(char *op, char *args)
 		return 0;
 
 		
+	else if(check_symbol(src) == FALSE || check_symbol(dest) == FALSE)
+		return 0;
 	/*         syntax check         */
     /*  							*/
 	/********************************/
